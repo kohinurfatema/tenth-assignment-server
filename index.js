0f9368375a9491d6b336cdc39cfca7e37467f607
kohinurfatema <kohinurfatema35@gmail.com>
2025-11-10 09:40:52 -0800

    mongodb connection

--- index.js
+++ index.js
@@ -69,3 +69,22 @@ const verifyJWTToken = (req, res, next) => {
         next();
     })
 }
+
+
+
+// ------------------------------------
+//         MONGODB CONNECTION
+// ------------------------------------
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vyznij5.mongodb.net/ecoTrackDB?appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+app.get('/', (req, res) => {
+    res.send('EcoTrack Server is running')
+})
